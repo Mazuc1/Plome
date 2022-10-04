abcebd51ece7a47a5f2c4a467f56a54f283e892d
Loïc Mazuc <mazuc.loic@icloud.com>
2022-10-04 21:53:19 +0200

    Initial commit

---
+++ Plome/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  Plome
+//
+//  Created by Loic Mazuc on 04/10/2022.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        true
+    }
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {}
+}
+
---
+++ Plome/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  Plome
+//
+//  Created by Loic Mazuc on 04/10/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
