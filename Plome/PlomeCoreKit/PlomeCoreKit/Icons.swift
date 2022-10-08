8353a0b09d26aa19ae255af3d4b90afcfea30c36
Loïc Mazuc <mazuc.loic@icloud.com>
2022-10-08 19:29:27 +0200

    feat: add icons

--- Plome/PlomeCoreKit/PlomeCoreKit/Icons.swift
+++ Plome/PlomeCoreKit/PlomeCoreKit/Icons.swift
@@ -9,6 +9,24 @@ import Foundation
 import UIKit
 
 public enum Icons {
+    // TabBar
+    case home
+    case model
+    case settings
+    
+    // Home
+    case list
+    case arrowUp
+    case arrowDown
+    case medal
+    
+    // Simulation
+    case success
+    case fail
+    case warning
+    
+    // Main
+    case trash
     case add
     
     public var uiImage: UIImage {
@@ -17,7 +35,21 @@ public enum Icons {
 
     public var name: String {
         switch self {
-        case .add: return "plus"
+        case .home: return "house"
+        case .model: return "cube.transparent"
+        case .settings: return "gearshape"
+            
+        case .list: return "list.bullet.clipboard"
+        case .arrowUp: return "arrow.up.circle.fill"
+        case .arrowDown: return "arrow.down.circle.fill"
+        case .medal: return "medal.fill"
+            
+        case .success: return "checkmark"
+        case .fail: return "xmark"
+        case .warning: return "exclamationmark.triangle.fill"
+            
+        case .trash: return "trash.circle"
+        case .add: return "plus.rectangle"
         }
     }
 }
