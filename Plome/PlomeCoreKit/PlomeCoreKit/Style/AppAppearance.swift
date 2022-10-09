b6b5ff509bcb1dc3dd055f4871355b6bd5e5ac15
Loïc Mazuc <mazuc.loic@icloud.com>
2022-10-09 18:22:03 +0200

    feat: set Tabbar appearance

--- Plome/PlomeCoreKit/PlomeCoreKit/Style/AppAppearance.swift
+++ Plome/PlomeCoreKit/PlomeCoreKit/Style/AppAppearance.swift
@@ -9,6 +9,10 @@ import UIKit
 
 public class AppAppearance {
     public static func setAppearance() {
-        
+        //  Tab Bar
+        let appearance = UITabBarAppearance()
+        appearance.configureWithOpaqueBackground()
+        UITabBar.appearance().standardAppearance = appearance
+        UITabBar.appearance().scrollEdgeAppearance = appearance
     }
 }
