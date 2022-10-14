f50d5ba4f549c558fe6244285fecac3827986700
Loïc Mazuc <mazuc.loic@icloud.com>
2022-10-14 09:41:53 +0200

    feat: create CDExam & CDSimulation

---
+++ Plome/PlomeCoreKit/PlomeCoreKit/CoreData/Model/CDExam.swift
@@ -0,0 +1,33 @@
+//
+//  CDExam+CoreDataProperties.swift
+//  
+//
+//  Created by Loic Mazuc on 14/10/2022.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc public enum ExamType: Int16 {
+    case trial
+    case option
+    case continuousControl
+}
+
+@objc(CDExam)
+public class CDExam: NSManagedObject {}
+
+extension CDExam {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<CDExam> {
+        return NSFetchRequest<CDExam>(entityName: "CDExam")
+    }
+
+    @NSManaged public var name: String?
+    @NSManaged public var coefficient: Float
+    @NSManaged public var grade: Float
+    @NSManaged public var type: ExamType
+    @NSManaged public var simulation: CDSimulation
+
+}
