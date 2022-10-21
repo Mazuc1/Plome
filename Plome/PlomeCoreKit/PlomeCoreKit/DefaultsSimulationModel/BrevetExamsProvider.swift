96d52b65e21d0c0feae6e80a4f7783507aceaec6
Loïc Mazuc <mazuc.loic@icloud.com>
2022-10-21 09:06:24 +0200

    refactor: set coefficient for brevet exams

--- Plome/PlomeCoreKit/PlomeCoreKit/DefaultsSimulationModel/BrevetExamsProvider.swift
+++ Plome/PlomeCoreKit/PlomeCoreKit/DefaultsSimulationModel/BrevetExamsProvider.swift
@@ -13,21 +13,21 @@ struct BrevetExamsProvider {
     }
     
     static private var continuousControls: [Exam] = [
-        .init(name: "Langue française", coefficient: nil, grade: nil, type: .continuousControl),
-        .init(name: "Langue étrangère", coefficient: nil, grade: nil, type: .continuousControl),
-        .init(name: "Langages scientifiques", coefficient: nil, grade: nil, type: .continuousControl),
-        .init(name: "Langages des arts", coefficient: nil, grade: nil, type: .continuousControl),
-        .init(name: "Outils pour apprendre", coefficient: nil, grade: nil, type: .continuousControl),
-        .init(name: "Formation du citoyen", coefficient: nil, grade: nil, type: .continuousControl),
-        .init(name: "Systèmes naturels et techniques", coefficient: nil, grade: nil, type: .continuousControl),
-        .init(name: "Représentations du monde", coefficient: nil, grade: nil, type: .continuousControl),
+        .init(name: "Langue française", coefficient: 1, grade: nil, type: .continuousControl),
+        .init(name: "Langue étrangère", coefficient: 1, grade: nil, type: .continuousControl),
+        .init(name: "Langages scientifiques", coefficient: 1, grade: nil, type: .continuousControl),
+        .init(name: "Langages des arts", coefficient: 1, grade: nil, type: .continuousControl),
+        .init(name: "Outils pour apprendre", coefficient: 1, grade: nil, type: .continuousControl),
+        .init(name: "Formation du citoyen", coefficient: 1, grade: nil, type: .continuousControl),
+        .init(name: "Systèmes naturels et techniques", coefficient: 1, grade: nil, type: .continuousControl),
+        .init(name: "Représentations du monde", coefficient: 1, grade: nil, type: .continuousControl),
     ]
     
     static private var trials: [Exam] = [
-        .init(name: "Français", coefficient: nil, grade: nil, type: .trial),
-        .init(name: "Mathématiques", coefficient: nil, grade: nil, type: .trial),
-        .init(name: "Oral", coefficient: nil, grade: nil, type: .trial),
-        .init(name: "Sciences", coefficient: nil, grade: nil, type: .trial),
-        .init(name: "Histoire-Géo-EMC", coefficient: nil, grade: nil, type: .trial),
+        .init(name: "Français", coefficient: 1, grade: nil, type: .trial),
+        .init(name: "Mathématiques", coefficient: 1, grade: nil, type: .trial),
+        .init(name: "Oral", coefficient: 1, grade: nil, type: .trial),
+        .init(name: "Sciences", coefficient: 1, grade: nil, type: .trial),
+        .init(name: "Histoire-Géo-EMC", coefficient: 1, grade: nil, type: .trial),
     ]
 }
