2fd305e6e4bf359fa6ee0cc22b62d3ba8bbb9ada
Loïc Mazuc <mazuc.loic@icloud.com>
2022-10-21 14:06:55 +0200

    feat: add default appearance for UINavigationBar

--- Plome/PlomeCoreKit/PlomeCoreKit/Style/AppAppearance.swift
+++ Plome/PlomeCoreKit/PlomeCoreKit/Style/AppAppearance.swift
@@ -10,9 +10,19 @@ import UIKit
 public class AppAppearance {
     public static func setAppearance() {
         //  Tab Bar
-        let appearance = UITabBarAppearance()
-        appearance.configureWithOpaqueBackground()
-        UITabBar.appearance().standardAppearance = appearance
-        UITabBar.appearance().scrollEdgeAppearance = appearance
+        let tabBarAppearance = UITabBarAppearance()
+        tabBarAppearance.configureWithOpaqueBackground()
+        UITabBar.appearance().standardAppearance = tabBarAppearance
+        UITabBar.appearance().scrollEdgeAppearance = tabBarAppearance
+        
+        //  Navigation Bar
+        let navigationBarAppearance = UINavigationBarAppearance()
+        navigationBarAppearance.configureWithOpaqueBackground()
+        navigationBarAppearance.titleTextAttributes = [.font: PlomeFont.subtitle.font, .foregroundColor: UIColor.black]
+        navigationBarAppearance.largeTitleTextAttributes = [.font: PlomeFont.title.font, .foregroundColor: UIColor.black]
+        
+        UINavigationBar.appearance().standardAppearance = navigationBarAppearance
+        UINavigationBar.appearance().scrollEdgeAppearance = navigationBarAppearance
+        UINavigationBar.appearance().tintColor = PlomeColor.pink.color
     }
 }
