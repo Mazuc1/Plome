d01899c4da3dd645fac2047569dd28cfc146cac1
Loïc Mazuc <mazuc.loic@icloud.com>
2022-10-21 14:07:35 +0200

    feat: add register button and table view

--- Plome/SimulationModel/AddSimulation/AddSimulationModelViewController.swift
+++ Plome/SimulationModel/AddSimulation/AddSimulationModelViewController.swift
@@ -16,6 +16,17 @@ final class AddSimulationModelViewController: AppViewController {
 
     // MARK: - UI
     
+    lazy var tableView = UITableView(frame: .zero, style: .plain).configure { [weak self] in
+        $0.delegate = self
+        $0.backgroundColor = .white
+        $0.separatorStyle = .none
+        $0.translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    lazy var primaryCTARegisterModel: PrimaryCTA = PrimaryCTA(title: "Enregistrer").configure { [weak self] in
+        $0.addTarget(self, action: #selector(userDidTapRegisterModel), for: .touchUpInside)
+        $0.translatesAutoresizingMaskIntoConstraints = false
+    }
     
     // MARK: - Init
     
@@ -33,11 +44,37 @@ final class AddSimulationModelViewController: AppViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.title = "Nouveau modèle"
+        
+        setupConstraint()
     }
     
     // MARK: - Methods
     
     private func setupConstraint() {
-       
+        view.addSubview(primaryCTARegisterModel)
+        
+        NSLayoutConstraint.activate([
+            primaryCTARegisterModel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: AppStyles.defaultSpacing(factor: 3)),
+            view.safeAreaLayoutGuide.bottomAnchor.constraint(equalTo: primaryCTARegisterModel.bottomAnchor, constant: AppStyles.defaultSpacing(factor: 1)),
+            view.trailingAnchor.constraint(equalTo: primaryCTARegisterModel.trailingAnchor, constant: AppStyles.defaultSpacing(factor: 3)),
+            primaryCTARegisterModel.heightAnchor.constraint(equalToConstant: PrimaryCTA.height),
+        ])
+        
+        view.addSubview(tableView)
+        
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: AppStyles.defaultSpacing(factor: 3)),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: AppStyles.defaultSpacing(factor: 3)),
+            view.trailingAnchor.constraint(equalTo: tableView.trailingAnchor, constant: AppStyles.defaultSpacing(factor: 3)),
+            primaryCTARegisterModel.topAnchor.constraint(equalTo: tableView.bottomAnchor, constant: AppStyles.defaultSpacing(factor: 2))
+        ])
+    }
+    
+    @objc private func userDidTapRegisterModel() {
+        print("🫑")
     }
 }
+
+// MARK: - Table View Delegate
+
+extension AddSimulationModelViewController: UITableViewDelegate {}
