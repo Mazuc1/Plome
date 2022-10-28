7f327e91d52441f635012cb833f2a72387b471ef
Loïc Mazuc <mazuc.loic@icloud.com>
2022-10-28 12:00:16 +0200

    feat: create SimulationModelsViewModelTests

---
+++ PlomeTests/SimulationModels/SimulationModelsViewModelTests.swift
@@ -0,0 +1,17 @@
+//
+//  SimulationModelsViewModelTests.swift
+//  PlomeTests
+//
+//  Created by Loic Mazuc on 28/10/2022.
+//
+
+import XCTest
+@testable import Plome
+@testable import PlomeCoreKit
+
+final class SimulationModelsViewModelTests: XCTestCase {
+    override class func setUp() {
+        super.setUp()
+    }
+
+}
