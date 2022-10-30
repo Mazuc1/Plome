96d6d8ad5596a99c773c6b237c5ce353d32d6c34
Loïc Mazuc <mazuc.loic@icloud.com>
2022-10-30 23:19:30 +0100

    feat: create SelectSimulationModelViewController

---
+++ Plome/Home/SelectSimulationModel/SelectSimulationModelViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SelectSimulationModelViewController.swift
+//  Plome
+//
+//  Created by Loic Mazuc on 30/10/2022.
+//
+
+import PlomeCoreKit
+import UIKit
+
+final class SelectSimulationModelViewController: AppViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
