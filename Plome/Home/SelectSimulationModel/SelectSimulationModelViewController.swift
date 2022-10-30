a0bc6c302e2e72921311534ba29b85fbc34e3000
Loïc Mazuc <mazuc.loic@icloud.com>
2022-10-30 23:22:19 +0100

    feat: create SelectSimulationModelViewModel

--- Plome/Home/SelectSimulationModel/SelectSimulationModelViewController.swift
+++ Plome/Home/SelectSimulationModel/SelectSimulationModelViewController.swift
@@ -9,6 +9,24 @@ import PlomeCoreKit
 import UIKit
 
 final class SelectSimulationModelViewController: AppViewController {
+    // MARK: - Properties
+
+    let viewModel: SelectSimulationModelViewModel
+
+    // MARK: - Init
+
+    required init(viewModel: SelectSimulationModelViewModel) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    @available(*, unavailable)
+    required init?(coder _: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: - Life cycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
     }
---
+++ Plome/Home/SelectSimulationModel/SelectSimulationModelViewModel.swift
@@ -0,0 +1,22 @@
+//
+//  SelectSimulationModelViewModel.swift
+//  Plome
+//
+//  Created by Loic Mazuc on 30/10/2022.
+//
+
+import Foundation
+
+final class SelectSimulationModelViewModel {
+    // MARK: - Properties
+
+    let router: SimulationsRouter
+
+    // MARK: - Init
+
+    init(router: SimulationsRouter) {
+        self.router = router
+    }
+
+    // MARK: - Methods
+}
