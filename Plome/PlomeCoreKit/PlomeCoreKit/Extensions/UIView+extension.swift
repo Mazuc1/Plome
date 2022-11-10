74be0c17c602dd5fdc8379e799eae1d27a5635b7
Loïc Mazuc <mazuc.loic@icloud.com>
2022-11-10 11:13:20 +0100

    feat: add UIView extension allows add separator

--- Plome/PlomeCoreKit/PlomeCoreKit/Extensions/UIView+extension.swift
+++ Plome/PlomeCoreKit/PlomeCoreKit/Extensions/UIView+extension.swift
@@ -42,3 +42,48 @@ public extension UIView {
         setNeedsLayout()
     }
 }
+
+public extension UIView {
+    enum SeparatorPosition {
+        case top
+        case bottom
+        case left
+        case right
+    }
+
+    @discardableResult
+    func addSeparator(at position: SeparatorPosition, color: UIColor, weight: CGFloat = 1.0 / UIScreen.main.scale, insets: UIEdgeInsets = .zero) -> UIView {
+        let view = UIView().configure {
+            $0.backgroundColor = color
+            $0.translatesAutoresizingMaskIntoConstraints = false
+        }
+        addSubview(view)
+
+        switch position {
+        case .top:
+            view.topAnchor.constraint(equalTo: topAnchor, constant: insets.top).isActive = true
+            view.leftAnchor.constraint(equalTo: leftAnchor, constant: insets.left).isActive = true
+            view.rightAnchor.constraint(equalTo: rightAnchor, constant: -insets.right).isActive = true
+            view.heightAnchor.constraint(equalToConstant: weight).isActive = true
+
+        case .bottom:
+            view.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -insets.bottom).isActive = true
+            view.leftAnchor.constraint(equalTo: leftAnchor, constant: insets.left).isActive = true
+            view.rightAnchor.constraint(equalTo: rightAnchor, constant: -insets.right).isActive = true
+            view.heightAnchor.constraint(equalToConstant: weight).isActive = true
+
+        case .left:
+            view.topAnchor.constraint(equalTo: topAnchor, constant: insets.top).isActive = true
+            view.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -insets.bottom).isActive = true
+            view.leftAnchor.constraint(equalTo: leftAnchor, constant: insets.left).isActive = true
+            view.widthAnchor.constraint(equalToConstant: weight).isActive = true
+
+        case .right:
+            view.topAnchor.constraint(equalTo: topAnchor, constant: insets.top).isActive = true
+            view.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -insets.bottom).isActive = true
+            view.rightAnchor.constraint(equalTo: rightAnchor, constant: -insets.right).isActive = true
+            view.widthAnchor.constraint(equalToConstant: weight).isActive = true
+        }
+        return view
+    }
+}
