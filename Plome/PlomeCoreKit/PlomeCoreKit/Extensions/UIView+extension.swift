681c09d1edff189dc9bfd8b87086479d256b4fbb
Loïc Mazuc <mazuc.loic@icloud.com>
2022-11-10 12:07:34 +0100

    feat: add UIView extension that make constraint for leading and trailing

--- Plome/PlomeCoreKit/PlomeCoreKit/Extensions/UIView+extension.swift
+++ Plome/PlomeCoreKit/PlomeCoreKit/Extensions/UIView+extension.swift
@@ -41,6 +41,34 @@ public extension UIView {
 
         setNeedsLayout()
     }
+
+    func attachToSides(parentView: UIView, edges: UIEdgeInsets = UIEdgeInsets.zero, size: CGSize? = nil, relativeToMargins: Bool = false) {
+        translatesAutoresizingMaskIntoConstraints = false
+        parentView.addSubview(self)
+
+        var attrs = [
+            NSLayoutConstraint.Attribute.right: (attr: NSLayoutConstraint.Attribute.right, const: edges.right * -1),
+            NSLayoutConstraint.Attribute.left: (attr: NSLayoutConstraint.Attribute.left, const: edges.left),
+        ]
+
+        if relativeToMargins {
+            attrs = [
+                NSLayoutConstraint.Attribute.right: (attr: NSLayoutConstraint.Attribute.rightMargin, const: edges.right * -1),
+                NSLayoutConstraint.Attribute.left: (attr: NSLayoutConstraint.Attribute.leftMargin, const: edges.left),
+            ]
+        }
+
+        for (fromAttr, to) in attrs {
+            parentView.addConstraint(NSLayoutConstraint(item: self, attribute: fromAttr, relatedBy: NSLayoutConstraint.Relation.equal, toItem: parentView, attribute: to.attr, multiplier: 1, constant: to.const))
+        }
+
+        if let size = size {
+            parentView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:[view(\(size.width))]", options: .alignAllCenterY, metrics: nil, views: ["view": self]))
+            parentView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:[view(\(size.height))]", options: .alignAllCenterX, metrics: nil, views: ["view": self]))
+        }
+
+        setNeedsLayout()
+    }
 }
 
 public extension UIView {
