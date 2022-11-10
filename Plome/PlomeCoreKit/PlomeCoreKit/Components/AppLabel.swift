0a6072fdcb00c2f0ea4355549446d2994b622292
Loïc Mazuc <mazuc.loic@icloud.com>
2022-11-10 12:06:57 +0100

    feat: create AppLabel

---
+++ Plome/PlomeCoreKit/PlomeCoreKit/Components/AppLabel.swift
@@ -0,0 +1,41 @@
+//
+//  AppLabel.swift
+//  PlomeCoreKit
+//
+//  Created by Loic Mazuc on 10/11/2022.
+//
+
+import Foundation
+import UIKit
+
+public class AppLabel: UILabel {
+    var topInset: CGFloat
+    var bottomInset: CGFloat
+    var leftInset: CGFloat
+    var rightInset: CGFloat
+
+    public required init(withInsets top: CGFloat, _ bottom: CGFloat, _ left: CGFloat, _ right: CGFloat) {
+        topInset = top
+        bottomInset = bottom
+        leftInset = left
+        rightInset = right
+        super.init(frame: CGRect.zero)
+    }
+
+    @available(*, unavailable)
+    required init?(coder _: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override public func drawText(in rect: CGRect) {
+        let insets = UIEdgeInsets(top: topInset, left: leftInset, bottom: bottomInset, right: rightInset)
+        super.drawText(in: rect.inset(by: insets))
+    }
+
+    override public var intrinsicContentSize: CGSize {
+        var contentSize = super.intrinsicContentSize
+        contentSize.height += topInset + bottomInset
+        contentSize.width += leftInset + rightInset
+        return contentSize
+    }
+}
