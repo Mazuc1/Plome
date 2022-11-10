cd3c9c86a78dda27833ea7c6089af2a96603bb55
Loïc Mazuc <mazuc.loic@icloud.com>
2022-11-10 10:25:50 +0100

    feat: add State to PrimaryCTA to apply different UI depend of his state

--- Plome/PlomeCoreKit/PlomeCoreKit/Components/PrimaryCTA.swift
+++ Plome/PlomeCoreKit/PlomeCoreKit/Components/PrimaryCTA.swift
@@ -8,17 +8,42 @@
 import UIKit
 
 public class PrimaryCTA: UIButton {
+    private enum State {
+        case disabled
+        case enabled
+    }
+
+    override public var isEnabled: Bool {
+        get {
+            return super.isEnabled
+        } set {
+            super.isEnabled = newValue
+            setStyle(for: newValue ? .enabled : .disabled)
+        }
+    }
+
     public required init(title: String) {
         super.init(frame: .zero)
         setTitle(title, for: .normal)
-        setTitleColor(.init(color: .lightViolet), for: .normal)
         titleLabel?.font = PlomeFont.demiBoldL.font
-        backgroundColor = .init(color: .lightViolet, alpha: 0.2)
         layer.cornerRadius = AppStyles.defaultRadius
+
+        setStyle(for: .enabled)
     }
 
     @available(*, unavailable)
     required init?(coder _: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+
+    private func setStyle(for state: State) {
+        switch state {
+        case .disabled:
+            setTitleColor(.init(color: .darkGray), for: .normal)
+            backgroundColor = .init(color: .darkGray, alpha: 0.2)
+        case .enabled:
+            setTitleColor(.init(color: .lightViolet), for: .normal)
+            backgroundColor = .init(color: .lightViolet, alpha: 0.2)
+        }
     }
 }
