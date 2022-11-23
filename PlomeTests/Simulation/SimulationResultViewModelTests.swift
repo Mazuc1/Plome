29ca90609db3ab3e9e7ec1c18b32eee1ac15b348
Loïc Mazuc <mazuc.loic@icloud.com>
2022-11-23 13:49:30 +0100

    feat: create SimulationResultViewModelTests

---
+++ PlomeTests/Simulation/SimulationResultViewModelTests.swift
@@ -0,0 +1,11 @@
+//
+//  SimulationResultViewModelTests.swift
+//  PlomeTests
+//
+//  Created by Loic Mazuc on 23/11/2022.
+//
+
+@testable import Plome
+import XCTest
+
+final class SimulationResultViewModelTests: XCTestCase {}
