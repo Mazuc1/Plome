b0a9a10ebce4eb4b3128fcc16e69a5d3bfc43878
Loïc Mazuc <mazuc.loic@icloud.com>
2022-11-24 11:06:09 +0100

    tests: test SelectSimulationModelViewModel

--- PlomeTests/Simulation/SelectSimulationModelViewModelTests.swift
+++ PlomeTests/Simulation/SelectSimulationModelViewModelTests.swift
@@ -5,6 +5,7 @@
 //  Created by Loic Mazuc on 23/11/2022.
 //
 
+import Combine
 @testable import Plome
 @testable import PlomeCoreKit
 import PlomeCoreKitTestsHelpers
@@ -12,8 +13,11 @@ import XCTest
 
 final class SelectSimulationModelViewModelTests: XCTestCase {
     private var simulationsRouter: SimulationsRouter!
+    private var selectSimulationModelViewModel: SelectSimulationModelViewModel!
     private var simulationRepository: CoreDataRepository<CDSimulation>!
     private var mockCoreData: MockCoreData!
+
+    private var cancellables: Set<AnyCancellable> = []
 
     override func setUp() {
         super.setUp()
@@ -22,7 +26,84 @@ final class SelectSimulationModelViewModelTests: XCTestCase {
         simulationRepository = CoreDataRepository(storageProvider: mockCoreData)
 
         simulationsRouter = SimulationsRouter(screens: .init(context: testContext), rootTransition: EmptyTransition())
+        selectSimulationModelViewModel = SelectSimulationModelViewModel(router: simulationsRouter, defaultSimulationModelsProvider: testContext.defaultSimulationModelsProvider, simulationRepository: simulationRepository)
     }
-    
-    // Même tests que SimulationModelsViewModelTests
+
+    // MARK: - updateSnapshot
+
+    func testWhenUpdatingSnapshotWithEmptyDatabaseThenSnapshotContainsOnlyDefaultSection() {
+        // Act
+        selectSimulationModelViewModel.updateSnapshot()
+
+        selectSimulationModelViewModel.$snapshot
+            .sink { snapshot in
+                // Assert
+                XCTAssertEqual(snapshot.sectionIdentifiers.count, 1)
+                XCTAssertEqual(snapshot.sectionIdentifiers[0], .default)
+            }
+            .store(in: &cancellables)
+    }
+
+    func testWhenUpdatingSnapshotWithDatabaseValuesThenSnapshotContainsTwoSection() {
+        // Arrange
+        try! simulationRepository.add { simulation, context in
+            simulation.name = "Test"
+            simulation.exams = .init()
+            let exam = CDExam(context: context)
+            exam.simulation = simulation
+            exam.name = "TestExam"
+            simulation.exams?.insert(exam)
+        }
+
+        // Act
+        selectSimulationModelViewModel.updateSnapshot()
+
+        selectSimulationModelViewModel.$snapshot
+            .sink { snapshot in
+                // Assert
+                XCTAssertEqual(snapshot.sectionIdentifiers.count, 2)
+                XCTAssertEqual(snapshot.sectionIdentifiers[0], .default)
+                XCTAssertEqual(snapshot.sectionIdentifiers[1], .coreData)
+            }
+            .store(in: &cancellables)
+    }
+
+    // MARK: - getSimulation
+
+    func testWhenGetCoreDataSimulationThenCoreDataSimulationIsReturned() {
+        // Arrange
+        try! simulationRepository.add { simulation, _ in
+            simulation.name = "Test model"
+        }
+
+        selectSimulationModelViewModel.updateSnapshot()
+
+        // Act
+        let simulation = selectSimulationModelViewModel.getSimulation(indexPath: .init(row: 0, section: 1))
+
+        // Assert
+        XCTAssertEqual(simulation!.name, "Test model")
+    }
+
+    func testWhenGetDefaultSimulationThenDefaultSimulationIsReturned() {
+        // Arrange
+        selectSimulationModelViewModel.updateSnapshot()
+
+        // Act
+        let simulation = selectSimulationModelViewModel.getSimulation(indexPath: .init(row: 2, section: 0))
+
+        // Assert
+        XCTAssertEqual(simulation!.name, "BAC Technologique")
+    }
+
+    func testWhenGetSimulationWithBadIndexPathSectionThenNilIsReturned() {
+        // Arrange
+        selectSimulationModelViewModel.updateSnapshot()
+
+        // Act
+        let simulation = selectSimulationModelViewModel.getSimulation(indexPath: .init(row: 0, section: 3))
+
+        // Assert
+        XCTAssertNil(simulation)
+    }
 }
--- PlomeTests/Simulation/SimulationViewModelTests.swift
+++ PlomeTests/Simulation/SimulationViewModelTests.swift
@@ -11,7 +11,6 @@ import PlomeCoreKitTestsHelpers
 import XCTest
 
 final class SimulationViewModelTests: XCTestCase {
-
     private var simulationsRouter: SimulationsRouter!
     private var simulationRepository: CoreDataRepository<CDSimulation>!
     private var mockCoreData: MockCoreData!
@@ -24,48 +23,48 @@ final class SimulationViewModelTests: XCTestCase {
 
         simulationsRouter = SimulationsRouter(screens: .init(context: testContext), rootTransition: EmptyTransition())
     }
-    
+
     // MARK: - userDidTapDeleteExam
-    
+
     func testWhenUserDeleteTrialExamThenExamIsDeleted() {
         // Arrange
         let simulation = Simulation(name: "", date: nil, exams: .init(), type: .generalBAC)
         simulation.add(exam: .init(name: "", coefficient: 1, grade: "13/20", type: .trial))
         let simulationViewModel = SimulationViewModel(router: simulationsRouter, simulation: simulation)
-        
+
         // Act
         simulationViewModel.userDidTapDeleteExam(at: .init(row: 0, section: 0))
-        
+
         // Assert
         XCTAssertTrue(simulationViewModel.simulation.exams(of: .trial).count == 0)
     }
-    
+
     func testWhenUserDeleteContinousControlExamThenExamIsDeleted() {
         // Arrange
         let simulation = Simulation(name: "", date: nil, exams: .init(), type: .generalBAC)
         simulation.add(exam: .init(name: "", coefficient: 1, grade: "13/20", type: .continuousControl))
         let simulationViewModel = SimulationViewModel(router: simulationsRouter, simulation: simulation)
-        
+
         // Act
         simulationViewModel.userDidTapDeleteExam(at: .init(row: 0, section: 1))
-        
+
         // Assert
         XCTAssertTrue(simulationViewModel.simulation.exams(of: .continuousControl).count == 0)
     }
-    
+
     func testWhenUserDeleteOptionExamThenExamIsDeleted() {
         // Arrange
         let simulation = Simulation(name: "", date: nil, exams: .init(), type: .generalBAC)
         simulation.add(exam: .init(name: "", coefficient: 1, grade: "13/20", type: .option))
         let simulationViewModel = SimulationViewModel(router: simulationsRouter, simulation: simulation)
-        
+
         // Act
         simulationViewModel.userDidTapDeleteExam(at: .init(row: 0, section: 2))
-        
+
         // Assert
         XCTAssertTrue(simulationViewModel.simulation.exams(of: .option).count == 0)
     }
-    
+
     func testThatDeleteExamFailsWhenIndexPathSectionIsNotCorrect() {
         // Arrange
         let simulation = Simulation(name: "", date: nil, exams: .init(), type: .generalBAC)
@@ -73,54 +72,54 @@ final class SimulationViewModelTests: XCTestCase {
         simulation.add(exam: .init(name: "", coefficient: 1, grade: "13/20", type: .trial))
         simulation.add(exam: .init(name: "", coefficient: 1, grade: "13/20", type: .continuousControl))
         let simulationViewModel = SimulationViewModel(router: simulationsRouter, simulation: simulation)
-        
+
         // Act
         simulationViewModel.userDidTapDeleteExam(at: .init(row: 0, section: 3))
-        
+
         // Assert
         XCTAssertTrue(simulationViewModel.simulation.exams!.count == 3)
     }
-    
+
     // MARK: - addExam
-    
+
     func testWhenAddTrialExamThenExamIsAdded() {
         // Arrange
         let simulation = Simulation(name: "", date: nil, exams: .init(), type: .generalBAC)
         let simulationViewModel = SimulationViewModel(router: simulationsRouter, simulation: simulation)
-        
+
         // Act
         simulationViewModel.addExam(name: "Test", in: .trial)
-        
+
         // Assert
         XCTAssertTrue(simulationViewModel.simulation.exams(of: .trial).count == 1)
     }
-    
+
     func testWhenAddContinuousControlExamThenExamIsAdded() {
         // Arrange
         let simulation = Simulation(name: "", date: nil, exams: .init(), type: .generalBAC)
         let simulationViewModel = SimulationViewModel(router: simulationsRouter, simulation: simulation)
-        
+
         // Act
         simulationViewModel.addExam(name: "Test", in: .continuousControl)
-        
+
         // Assert
         XCTAssertTrue(simulationViewModel.simulation.exams(of: .continuousControl).count == 1)
     }
-    
+
     func testWhenAddOptionExamThenExamIsAdded() {
         // Arrange
         let simulation = Simulation(name: "", date: nil, exams: .init(), type: .generalBAC)
         let simulationViewModel = SimulationViewModel(router: simulationsRouter, simulation: simulation)
-        
+
         // Act
         simulationViewModel.addExam(name: "Test", in: .option)
-        
+
         // Assert
         XCTAssertTrue(simulationViewModel.simulation.exams(of: .option).count == 1)
     }
-       
+
     // MARK: - userDidChangeValue
-    
+
     func testWhenMissingOneOrMoreExamGradeThenCalculationIsDisabled() {
         // Arrange
         let simulation = Simulation(name: "", date: nil, exams: .init(), type: .generalBAC)
@@ -128,14 +127,14 @@ final class SimulationViewModelTests: XCTestCase {
         simulation.add(exam: .init(name: "", coefficient: 1, grade: nil, type: .trial))
         simulation.add(exam: .init(name: "", coefficient: 1, grade: "13/20", type: .continuousControl))
         let simulationViewModel = SimulationViewModel(router: simulationsRouter, simulation: simulation)
-        
+
         // Act
         simulationViewModel.userDidChangeValue()
-        
+
         // Assert
         XCTAssertFalse(simulationViewModel.canCalculate)
     }
-    
+
     func testWhenAllExamGradeAreFilledThenCalculationIsEnabled() {
         // Arrange
         let simulation = Simulation(name: "", date: nil, exams: .init(), type: .generalBAC)
@@ -143,10 +142,10 @@ final class SimulationViewModelTests: XCTestCase {
         simulation.add(exam: .init(name: "", coefficient: 1, grade: "12/20", type: .trial))
         simulation.add(exam: .init(name: "", coefficient: 1, grade: "13/20", type: .continuousControl))
         let simulationViewModel = SimulationViewModel(router: simulationsRouter, simulation: simulation)
-        
+
         // Act
         simulationViewModel.userDidChangeValue()
-        
+
         // Assert
         XCTAssertTrue(simulationViewModel.canCalculate)
     }
