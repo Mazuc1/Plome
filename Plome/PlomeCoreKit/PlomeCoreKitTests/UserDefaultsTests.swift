e89ed944cde8331d488f457dae3c63e2a0c9188b
Loïc Mazuc <mazuc.loic@icloud.com>
2022-12-02 11:37:43 +0100

    tests: test UserDefaults

---
+++ Plome/PlomeCoreKit/PlomeCoreKitTests/UserDefaultsTests.swift
@@ -0,0 +1,41 @@
+//
+//  UserDefaultsTests.swift
+//  PlomeCoreKitTests
+//
+//  Created by Loic Mazuc on 02/12/2022.
+//
+
+@testable import PlomeCoreKit
+import PlomeCoreKitTestsHelpers
+import XCTest
+
+final class UserDefaultsTests: XCTestCase {
+    private var userDefauls: DefaultsProtocol!
+
+    override func setUp() {
+        super.setUp()
+        userDefauls = Defaults(userDefaults: .init(suiteName: "UserDefaultTests")!)
+    }
+
+    func testSetData() {
+        // Arrange
+        userDefauls.setData(value: true, key: .userDefaultTest)
+
+        // Act
+        let result = userDefauls.getData(type: Bool.self, forKey: .userDefaultTest)
+
+        // Assert
+        XCTAssertEqual(result, true)
+    }
+
+    func testThatGetDataReturnsNilWhenNoDataHaveBeenSavedBefore() {
+        // Arrange
+        userDefauls.removeData(key: .userDefaultTest)
+
+        // Act
+        let result = userDefauls.getData(type: Bool.self, forKey: .userDefaultTest)
+
+        // Assert
+        XCTAssertNil(result)
+    }
+}
