7359cac8e34351c1a7cba4dba4f82acafe83c999
Loïc Mazuc <mazuc.loic@icloud.com>
2022-12-09 22:42:53 +0100

    refactor: renaming swiftgen.yml to plomecorekit-config.yml

--- Plome/PlomeCoreKit/PlomeCoreKit/Ressources/L10n.swift
+++ Plome/PlomeCoreKit/PlomeCoreKit/Ressources/L10n.swift
@@ -10,155 +10,153 @@ import Foundation
 // swiftlint:disable explicit_type_interface function_parameter_count identifier_name line_length
 // swiftlint:disable nesting type_body_length type_name vertical_whitespace_opening_braces
 public enum L10n {
-    /// 1ère Spécialité abandonnée
-    public static let _1AbandonedSpeciality = L10n.tr("PlomeCoreKit", "1_abandoned_speciality", fallback: "1ère Spécialité abandonnée")
-    /// 1ère EMC
-    public static let _1Emc = L10n.tr("PlomeCoreKit", "1_EMC", fallback: "1ère EMC")
-    /// 1ère EPS
-    public static let _1Eps = L10n.tr("PlomeCoreKit", "1_EPS", fallback: "1ère EPS")
-    /// 1ère Histoire-géo
-    public static let _1HistoryGeography = L10n.tr("PlomeCoreKit", "1_history_geography", fallback: "1ère Histoire-géo")
-    /// 1ère LVA
-    public static let _1Lva = L10n.tr("PlomeCoreKit", "1_LVA", fallback: "1ère LVA")
-    /// 1ère LVB
-    public static let _1Lvb = L10n.tr("PlomeCoreKit", "1_LVB", fallback: "1ère LVB")
-    /// 1ère Enseignement scientifique
-    public static let _1ScientificEducation = L10n.tr("PlomeCoreKit", "1_scientific_education", fallback: "1ère Enseignement scientifique")
-    /// Langages des arts
-    public static let artLanguage = L10n.tr("PlomeCoreKit", "art_language", fallback: "Langages des arts")
-    /// Brevet
-    public static let brevet = L10n.tr("PlomeCoreKit", "brevet", fallback: "Brevet")
-    /// Formation du citoyen
-    public static let citizenFormation = L10n.tr("PlomeCoreKit", "citizen_formation", fallback: "Formation du citoyen")
-    /// Contrôle continue
-    public static let continuousControlsType = L10n.tr("PlomeCoreKit", "continuousControls_type", fallback: "Contrôle continue")
-    /// Vous ne pouvez pas avoir de mention en dessous de la moyenne
-    public static let disableMention = L10n.tr("PlomeCoreKit", "disable_mention", fallback: "Vous ne pouvez pas avoir de mention en dessous de la moyenne")
-    /// Vous n'êtes pas admis 😕
-    public static let failureAdmission = L10n.tr("PlomeCoreKit", "failure_admission", fallback: "Vous n'êtes pas admis 😕")
-    /// Oups...
-    public static let failureResult = L10n.tr("PlomeCoreKit", "failure_result", fallback: "Oups...")
-    /// Langue étrangère
-    public static let foreignLanguage = L10n.tr("PlomeCoreKit", "foreign_language", fallback: "Langue étrangère")
-    /// Français
-    public static let french = L10n.tr("PlomeCoreKit", "french", fallback: "Français")
-    /// Langue française
-    public static let frenchLanguage = L10n.tr("PlomeCoreKit", "french_language", fallback: "Langue française")
-    /// Français oral
-    public static let frenchOral = L10n.tr("PlomeCoreKit", "french_oral", fallback: "Français oral")
-    /// Français écrit
-    public static let frenchWritting = L10n.tr("PlomeCoreKit", "french_writting", fallback: "Français écrit")
-    /// BAC Général
-    public static let generalBAC = L10n.tr("PlomeCoreKit", "general_BAC", fallback: "BAC Général")
-    /// Grand oral
-    public static let grandOral = L10n.tr("PlomeCoreKit", "grand_oral", fallback: "Grand oral")
-    /// Great mention
-    public static let greatMention = L10n.tr("PlomeCoreKit", "great_mention", fallback: "Great mention")
-    /// Histoire-Géo-EMC
-    public static let historyGeographyEMC = L10n.tr("PlomeCoreKit", "history_geography_EMC", fallback: "Histoire-Géo-EMC")
-    /// Outils pour apprendre
-    public static let learningTools = L10n.tr("PlomeCoreKit", "learning_tools", fallback: "Outils pour apprendre")
-    /// Mathématiques
-    public static let math = L10n.tr("PlomeCoreKit", "math", fallback: "Mathématiques")
-    /// Systèmes naturels et techniques
-    public static let natualTechnologicSystem = L10n.tr("PlomeCoreKit", "natual_technologic_system", fallback: "Systèmes naturels et techniques")
-    /// Option
-    public static let optionsType = L10n.tr("PlomeCoreKit", "options_type", fallback: "Option")
-    /// Oral
-    public static let oral = L10n.tr("PlomeCoreKit", "oral", fallback: "Oral")
-    /// Philosophie
-    public static let philosophy = L10n.tr("PlomeCoreKit", "philosophy", fallback: "Philosophie")
-    /// Quite well mention
-    public static let quiteWellMention = L10n.tr("PlomeCoreKit", "quite_well_mention", fallback: "Quite well mention")
-    /// Sciences
-    public static let sciences = L10n.tr("PlomeCoreKit", "sciences", fallback: "Sciences")
-    /// Langages scientifiques
-    public static let scientistLanguage = L10n.tr("PlomeCoreKit", "scientist_language", fallback: "Langages scientifiques")
-    /// Enseignement spécialité 1
-    public static let specializedEducation1 = L10n.tr("PlomeCoreKit", "specialized_education_1", fallback: "Enseignement spécialité 1")
-    /// Enseignement spécialité 2
-    public static let specializedEducation2 = L10n.tr("PlomeCoreKit", "specialized_education_2", fallback: "Enseignement spécialité 2")
-    /// Vous êtes admis ! 🥳
-    public static let successAdmission = L10n.tr("PlomeCoreKit", "success_admission", fallback: "Vous êtes admis ! 🥳")
-    /// Félicitation !
-    public static let successResult = L10n.tr("PlomeCoreKit", "success_result", fallback: "Félicitation !")
-    /// Terminale EMC
-    public static let tEmc = L10n.tr("PlomeCoreKit", "t_EMC", fallback: "Terminale EMC")
-    /// Terminale EPS
-    public static let tEps = L10n.tr("PlomeCoreKit", "t_EPS", fallback: "Terminale EPS")
-    /// Terminale Histoire-géo
-    public static let tHistoryGeography = L10n.tr("PlomeCoreKit", "t_history_geography", fallback: "Terminale Histoire-géo")
-    /// Terminale LVA
-    public static let tLva = L10n.tr("PlomeCoreKit", "t_LVA", fallback: "Terminale LVA")
-    /// Terminale LVB
-    public static let tLvb = L10n.tr("PlomeCoreKit", "t_LVB", fallback: "Terminale LVB")
-    /// Terminale mathématique
-    public static let tMath = L10n.tr("PlomeCoreKit", "t_math", fallback: "Terminale mathématique")
-    /// Terminale Enseignement scientifique
-    public static let tScientificEducation = L10n.tr("PlomeCoreKit", "t_scientific_education", fallback: "Terminale Enseignement scientifique")
-    /// BAC Technologique
-    public static let technologicalBAC = L10n.tr("PlomeCoreKit", "technological_BAC", fallback: "BAC Technologique")
-    /// Épreuve
-    public static let trialsType = L10n.tr("PlomeCoreKit", "trials_type", fallback: "Épreuve")
-    /// Oups, il y a eu un imprévu...
-    public static let unexpected = L10n.tr("PlomeCoreKit", "unexpected", fallback: "Oups, il y a eu un imprévu...")
-    /// Very great mention
-    public static let veryGreatMention = L10n.tr("PlomeCoreKit", "very_great_mention", fallback: "Very great mention")
-    /// Without mention
-    public static let withoutMention = L10n.tr("PlomeCoreKit", "without_mention", fallback: "Without mention")
-    /// Représentations du monde
-    public static let worldRepresentation = L10n.tr("PlomeCoreKit", "world_representation", fallback: "Représentations du monde")
-    /// Ecrivez ici...
-    public static let writeHere = L10n.tr("PlomeCoreKit", "write_here", fallback: "Ecrivez ici...")
-    public enum General {
-        /// Ajouter
-        public static let add = L10n.tr("PlomeCoreKit", "general.add", fallback: "Ajouter")
-        /// Annuler
-        public static let cancel = L10n.tr("PlomeCoreKit", "general.cancel", fallback: "Annuler")
-        /// Une erreur est survenue 😕
-        public static let commonErrorMessage = L10n.tr("PlomeCoreKit", "general.common_error_message", fallback: "Une erreur est survenue 😕")
-        /// Terminé
-        public static let done = L10n.tr("PlomeCoreKit", "general.done", fallback: "Terminé")
-        /// FR-fr
-        public static let locale = L10n.tr("PlomeCoreKit", "general.locale", fallback: "FR-fr")
-        /// Nouveau
-        public static let new = L10n.tr("PlomeCoreKit", "general.new", fallback: "Nouveau")
-        /// Non
-        public static let no = L10n.tr("PlomeCoreKit", "general.no", fallback: "Non")
-        /// Ok
-        public static let ok = L10n.tr("PlomeCoreKit", "general.ok", fallback: "Ok")
-        /// Oups
-        public static let oups = L10n.tr("PlomeCoreKit", "general.oups", fallback: "Oups")
-        /// Enregistrer
-        public static let save = L10n.tr("PlomeCoreKit", "general.save", fallback: "Enregistrer")
-        /// Attention
-        public static let warning = L10n.tr("PlomeCoreKit", "general.warning", fallback: "Attention")
-        /// Oui
-        public static let yes = L10n.tr("PlomeCoreKit", "general.yes", fallback: "Oui")
-    }
+  /// 1ère Abandonned speciality
+  public static let _1AbandonedSpeciality = L10n.tr("PlomeCoreKit", "1_abandoned_speciality", fallback: "1ère Abandonned speciality")
+  /// 1ère EMC
+  public static let _1Emc = L10n.tr("PlomeCoreKit", "1_EMC", fallback: "1ère EMC")
+  /// 1ère EPS
+  public static let _1Eps = L10n.tr("PlomeCoreKit", "1_EPS", fallback: "1ère EPS")
+  /// 1ère History-geography
+  public static let _1HistoryGeography = L10n.tr("PlomeCoreKit", "1_history_geography", fallback: "1ère History-geography")
+  /// 1ère LVA
+  public static let _1Lva = L10n.tr("PlomeCoreKit", "1_LVA", fallback: "1ère LVA")
+  /// 1ère LVB
+  public static let _1Lvb = L10n.tr("PlomeCoreKit", "1_LVB", fallback: "1ère LVB")
+  /// 1ère Scientific education
+  public static let _1ScientificEducation = L10n.tr("PlomeCoreKit", "1_scientific_education", fallback: "1ère Scientific education")
+  /// Arts language
+  public static let artLanguage = L10n.tr("PlomeCoreKit", "art_language", fallback: "Arts language")
+  /// Brevet
+  public static let brevet = L10n.tr("PlomeCoreKit", "brevet", fallback: "Brevet")
+  /// Citizen formation
+  public static let citizenFormation = L10n.tr("PlomeCoreKit", "citizen_formation", fallback: "Citizen formation")
+  /// Continuous controls
+  public static let continuousControlsType = L10n.tr("PlomeCoreKit", "continuousControls_type", fallback: "Continuous controls")
+  /// You cannot have a mention below the average
+  public static let disableMention = L10n.tr("PlomeCoreKit", "disable_mention", fallback: "You cannot have a mention below the average")
+  /// You are not admitted 😕
+  public static let failureAdmission = L10n.tr("PlomeCoreKit", "failure_admission", fallback: "You are not admitted 😕")
+  /// Whoops...
+  public static let failureResult = L10n.tr("PlomeCoreKit", "failure_result", fallback: "Whoops...")
+  /// Foreign language
+  public static let foreignLanguage = L10n.tr("PlomeCoreKit", "foreign_language", fallback: "Foreign language")
+  /// French
+  public static let french = L10n.tr("PlomeCoreKit", "french", fallback: "French")
+  /// French language
+  public static let frenchLanguage = L10n.tr("PlomeCoreKit", "french_language", fallback: "French language")
+  /// French oral
+  public static let frenchOral = L10n.tr("PlomeCoreKit", "french_oral", fallback: "French oral")
+  /// French writting
+  public static let frenchWritting = L10n.tr("PlomeCoreKit", "french_writting", fallback: "French writting")
+  /// BAC Général
+  public static let generalBAC = L10n.tr("PlomeCoreKit", "general_BAC", fallback: "BAC Général")
+  /// Grand oral
+  public static let grandOral = L10n.tr("PlomeCoreKit", "grand_oral", fallback: "Grand oral")
+  /// Great mention
+  public static let greatMention = L10n.tr("PlomeCoreKit", "great_mention", fallback: "Great mention")
+  /// History-Geography-EMC
+  public static let historyGeographyEMC = L10n.tr("PlomeCoreKit", "history_geography_EMC", fallback: "History-Geography-EMC")
+  /// Learning tools
+  public static let learningTools = L10n.tr("PlomeCoreKit", "learning_tools", fallback: "Learning tools")
+  /// Mathematics
+  public static let math = L10n.tr("PlomeCoreKit", "math", fallback: "Mathematics")
+  /// Natural and technological system
+  public static let natualTechnologicSystem = L10n.tr("PlomeCoreKit", "natual_technologic_system", fallback: "Natural and technological system")
+  /// Options
+  public static let optionsType = L10n.tr("PlomeCoreKit", "options_type", fallback: "Options")
+  /// Oral
+  public static let oral = L10n.tr("PlomeCoreKit", "oral", fallback: "Oral")
+  /// Philosophy
+  public static let philosophy = L10n.tr("PlomeCoreKit", "philosophy", fallback: "Philosophy")
+  /// Quite well mention
+  public static let quiteWellMention = L10n.tr("PlomeCoreKit", "quite_well_mention", fallback: "Quite well mention")
+  /// Sciences
+  public static let sciences = L10n.tr("PlomeCoreKit", "sciences", fallback: "Sciences")
+  /// Scientist language
+  public static let scientistLanguage = L10n.tr("PlomeCoreKit", "scientist_language", fallback: "Scientist language")
+  /// Specialized education 1
+  public static let specializedEducation1 = L10n.tr("PlomeCoreKit", "specialized_education_1", fallback: "Specialized education 1")
+  /// Specialized education 2
+  public static let specializedEducation2 = L10n.tr("PlomeCoreKit", "specialized_education_2", fallback: "Specialized education 2")
+  /// You are admitted! 🥳
+  public static let successAdmission = L10n.tr("PlomeCoreKit", "success_admission", fallback: "You are admitted! 🥳")
+  /// Congratulation !
+  public static let successResult = L10n.tr("PlomeCoreKit", "success_result", fallback: "Congratulation !")
+  /// Terminale EMC
+  public static let tEmc = L10n.tr("PlomeCoreKit", "t_EMC", fallback: "Terminale EMC")
+  /// Terminale EPS
+  public static let tEps = L10n.tr("PlomeCoreKit", "t_EPS", fallback: "Terminale EPS")
+  /// Terminale History-geography
+  public static let tHistoryGeography = L10n.tr("PlomeCoreKit", "t_history_geography", fallback: "Terminale History-geography")
+  /// Terminale LVA
+  public static let tLva = L10n.tr("PlomeCoreKit", "t_LVA", fallback: "Terminale LVA")
+  /// Terminale LVB
+  public static let tLvb = L10n.tr("PlomeCoreKit", "t_LVB", fallback: "Terminale LVB")
+  /// Terminale mathematics
+  public static let tMath = L10n.tr("PlomeCoreKit", "t_math", fallback: "Terminale mathematics")
+  /// Terminale scientific education
+  public static let tScientificEducation = L10n.tr("PlomeCoreKit", "t_scientific_education", fallback: "Terminale scientific education")
+  /// BAC Technologique
+  public static let technologicalBAC = L10n.tr("PlomeCoreKit", "technological_BAC", fallback: "BAC Technologique")
+  /// Trials
+  public static let trialsType = L10n.tr("PlomeCoreKit", "trials_type", fallback: "Trials")
+  /// Whoops, there was an unexpected error...
+  public static let unexpected = L10n.tr("PlomeCoreKit", "unexpected", fallback: "Whoops, there was an unexpected error...")
+  /// Very great mention
+  public static let veryGreatMention = L10n.tr("PlomeCoreKit", "very_great_mention", fallback: "Very great mention")
+  /// Without mention
+  public static let withoutMention = L10n.tr("PlomeCoreKit", "without_mention", fallback: "Without mention")
+  /// World representation
+  public static let worldRepresentation = L10n.tr("PlomeCoreKit", "world_representation", fallback: "World representation")
+  /// Write here...
+  public static let writeHere = L10n.tr("PlomeCoreKit", "write_here", fallback: "Write here...")
+  public enum General {
+    /// Add
+    public static let add = L10n.tr("PlomeCoreKit", "general.add", fallback: "Add")
+    /// Cancel
+    public static let cancel = L10n.tr("PlomeCoreKit", "general.cancel", fallback: "Cancel")
+    /// An error has occurred 😕
+    public static let commonErrorMessage = L10n.tr("PlomeCoreKit", "general.common_error_message", fallback: "An error has occurred 😕")
+    /// Done
+    public static let done = L10n.tr("PlomeCoreKit", "general.done", fallback: "Done")
+    /// En-en
+    public static let locale = L10n.tr("PlomeCoreKit", "general.locale", fallback: "En-en")
+    /// New
+    public static let new = L10n.tr("PlomeCoreKit", "general.new", fallback: "New")
+    /// No
+    public static let no = L10n.tr("PlomeCoreKit", "general.no", fallback: "No")
+    /// Ok
+    public static let ok = L10n.tr("PlomeCoreKit", "general.ok", fallback: "Ok")
+    /// Whoops
+    public static let oups = L10n.tr("PlomeCoreKit", "general.oups", fallback: "Whoops")
+    /// Save
+    public static let save = L10n.tr("PlomeCoreKit", "general.save", fallback: "Save")
+    /// Warning
+    public static let warning = L10n.tr("PlomeCoreKit", "general.warning", fallback: "Warning")
+    /// Yes
+    public static let yes = L10n.tr("PlomeCoreKit", "general.yes", fallback: "Yes")
+  }
 }
-
 // swiftlint:enable explicit_type_interface function_parameter_count identifier_name line_length
 // swiftlint:enable nesting type_body_length type_name vertical_whitespace_opening_braces
 
 // MARK: - Implementation Details
 
 extension L10n {
-    private static func tr(_ table: String, _ key: String, _ args: CVarArg..., fallback value: String) -> String {
-        let format = BundleToken.bundle.localizedString(forKey: key, value: value, table: table)
-        return String(format: format, locale: Locale.current, arguments: args)
-    }
+  private static func tr(_ table: String, _ key: String, _ args: CVarArg..., fallback value: String) -> String {
+    let format = BundleToken.bundle.localizedString(forKey: key, value: value, table: table)
+    return String(format: format, locale: Locale.current, arguments: args)
+  }
 }
 
 // swiftlint:disable convenience_type
 private final class BundleToken {
-    static let bundle: Bundle = {
-        #if SWIFT_PACKAGE
-            return Bundle.module
-        #else
-            return Bundle(for: BundleToken.self)
-        #endif
-    }()
+  static let bundle: Bundle = {
+    #if SWIFT_PACKAGE
+    return Bundle.module
+    #else
+    return Bundle(for: BundleToken.self)
+    #endif
+  }()
 }
-
 // swiftlint:enable convenience_type
