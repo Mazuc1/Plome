ed243a9b73af8465b2ec1c8a043c282bc16edbcc
Loïc Mazuc <mazuc.loic@icloud.com>
2022-12-16 11:32:36 +0100

    feat: display onboarding at first launch

--- Plome/Onboarding/OnboardingFlowController.swift
+++ Plome/Onboarding/OnboardingFlowController.swift
@@ -18,20 +18,27 @@ final class OnboardingFlowController {
     // MARK: - Properties
 
     let screens: Screens
+    let userDefaults: Defaults
     var onFinished: (() -> Void)?
 
     var mainRouter: OnboardingRouter
 
     // MARK: - Init
 
-    init(screens: Screens) {
+    init(screens: Screens, userDefaults: Defaults) {
         self.screens = screens
+        self.userDefaults = userDefaults
         mainRouter = OnboardingRouter(screens: screens, rootTransition: EmptyTransition())
         mainRouter.mainRouterDelegate = self
     }
 
     func start() -> UIViewController {
         return mainRouter.makeRootViewController()
+    }
+
+    func shouldPresentOnboarding() -> Bool {
+        // defaults.setData(value: false, key: .onboardingHasBeenSeen)
+        return userDefaults.getData(type: Bool.self, forKey: .hasOnboardingBeenSeen) ?? false
     }
 }
 
