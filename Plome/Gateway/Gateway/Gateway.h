58b591483b6a4e7d05111adc12f6efd75e7cb9f2
Loïc Mazuc <mazuc.loic@icloud.com>
2022-12-28 00:56:49 +0100

    chore: create Gateway module

---
+++ Plome/Gateway/Gateway/Gateway.h
@@ -0,0 +1,18 @@
+//
+//  Gateway.h
+//  Gateway
+//
+//  Created by Loic Mazuc on 28/12/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Gateway.
+FOUNDATION_EXPORT double GatewayVersionNumber;
+
+//! Project version string for Gateway.
+FOUNDATION_EXPORT const unsigned char GatewayVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Gateway/PublicHeader.h>
+
+
