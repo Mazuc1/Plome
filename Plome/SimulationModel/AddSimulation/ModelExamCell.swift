e58eb5b419742004fbf7ad30e12816ca72f57a83
Loïc Mazuc <mazuc.loic@icloud.com>
2022-12-27 21:30:03 +0100

    feat: add coefficient text field in simulation model

--- Plome/SimulationModel/AddSimulation/ModelExamCell.swift
+++ Plome/SimulationModel/AddSimulation/ModelExamCell.swift
@@ -5,6 +5,7 @@
 //  Created by Loic Mazuc on 23/10/2022.
 //
 
+import MaterialComponents
 import PlomeCoreKit
 import UIKit
 
@@ -20,14 +21,28 @@ final class ModelExamCell: UITableViewCell {
         $0.font = PlomeFont.bodyL.font
         $0.textColor = PlomeColor.darkBlue.color
         $0.textAlignment = .left
-        $0.numberOfLines = 1
+        $0.numberOfLines = 0
         $0.translatesAutoresizingMaskIntoConstraints = false
     }
 
+    private let textFieldCoeff: MDCOutlinedTextField = .init().configure {
+        $0.label.text = L10n.Home.coeff
+        $0.placeholder = L10n.Home.coeffPlaceholder
+        $0.font = PlomeFont.bodyM.font
+        $0.verticalDensity = 30
+        $0.setOutlineColor(.lightGray, for: .normal)
+        $0.setNormalLabelColor(.lightGray, for: .normal)
+        $0.sizeToFit()
+        $0.keyboardType = .numbersAndPunctuation
+        $0.returnKeyType = .done
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        $0.widthAnchor.constraint(equalToConstant: 100).isActive = true
+    }
+
     private var stackView: UIStackView = .init().configure {
-        $0.axis = .vertical
-        $0.alignment = .leading
-        $0.distribution = .equalSpacing
+        $0.axis = .horizontal
+        $0.alignment = .center
+        $0.distribution = .fill
         $0.spacing = AppStyles.defaultSpacing(factor: 0.5)
         $0.backgroundColor = .white
         $0.layer.cornerRadius = AppStyles.defaultRadius
@@ -51,6 +66,7 @@ final class ModelExamCell: UITableViewCell {
         setupLayout()
 
         labelExamName.text = exam.name
+        textFieldCoeff.text = "\(exam.coefficient ?? 1.0)"
 
         backgroundColor = .clear
         selectionStyle = .none
@@ -58,14 +74,13 @@ final class ModelExamCell: UITableViewCell {
 
     private func setupLayout() {
         contentView.addSubview(stackView)
-        stackView.addArrangedSubviews([labelExamName])
+        stackView.addArrangedSubviews([labelExamName, textFieldCoeff])
 
         NSLayoutConstraint.activate([
-            stackView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: 0),
-            stackView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 0),
+            stackView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+            stackView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
             stackView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: AppStyles.defaultSpacing),
-            stackView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: 0),
-            stackView.heightAnchor.constraint(equalToConstant: Self.modelExamCellHeight),
+            stackView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
         ])
 
         stackView.layoutMargins = .init(top: AppStyles.defaultSpacing,
