49435e765ca67b816f2b9480333d33aabc6f514d
Loïc Mazuc <mazuc.loic@icloud.com>
2023-01-01 22:36:14 +0100

    refactor: remove LoadingVC

--- Plome/PlomeCoreKit/PlomeCoreKit/Components/AppViewController.swift
+++ Plome/PlomeCoreKit/PlomeCoreKit/Components/AppViewController.swift
@@ -8,36 +8,8 @@
 import UIKit
 
 open class AppViewController: UIViewController {
-    
-    // MARK: - Properties
-    
-    private var loadingVC: LoadingViewController?
-    
-    // MARK: - Life cycle
-    
     override open func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = PlomeColor.background.color
-    }
-    
-    // MARK: - Methods
-    
-    public func showLoader() {
-        if loadingVC == nil { createLoadingVC() }
-        
-        if let loadingVC {
-            present(loadingVC, animated: true, completion: nil)
-        }
-    }
-    
-    public func dismissLoader() {
-        loadingVC?.dismiss(animated: true)
-    }
-    
-    private func createLoadingVC() {
-        loadingVC = .init().configure {
-            $0.modalPresentationStyle = .overCurrentContext
-            $0.modalTransitionStyle = .crossDissolve
-        }
     }
 }
