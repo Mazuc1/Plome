9f6c309c5fff55eb75c38be0a71f9c845619052d
Loïc Mazuc <mazuc.loic@icloud.com>
2023-01-04 22:18:42 +0100

    test: test multipart form data request + task loader manager

---
+++ Plome/Gateway/GatewayTests/Helpers/TestEndPoint.swift
@@ -0,0 +1,32 @@
+//
+//  TestEndPoint.swift
+//  GatewayTests
+//
+//  Created by Loic Mazuc on 02/01/2023.
+//
+
+import Foundation
+@testable import Gateway
+
+enum TestEndPoint {
+    case test(someParameter: String)
+    case dataTest
+
+    var endPoint: EndPoint {
+        switch self {
+        case let .test(someParameter):
+            return .init(method: .GET,
+                         host: "google.com",
+                         path: "/\(someParameter)",
+                         parameters: nil,
+                         body: ["bodyName": "bodyContent"])
+            
+        case .dataTest:
+            return .init(method: .GET,
+                         host: "google.com",
+                         path: "/",
+                         parameters: nil,
+                         body: ["bodyName": Data("Data".utf8)])
+        }
+    }
+}
