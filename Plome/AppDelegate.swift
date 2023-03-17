bf796871d577e21ed87a9f995f81ef61c136608e
Loïc Mazuc <mazuc.loic@icloud.com>
2023-03-17 16:07:25 +0100

    chore: remove unused package

--- Plome/AppDelegate.swift
+++ Plome/AppDelegate.swift
@@ -12,16 +12,16 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_: UIApplication, didFinishLaunchingWithOptions _: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // FirebaseApp.configure()
+        FirebaseApp.configure()
         AppAppearance.setKeyboardAppearance()
-        // Analytics.setAnalyticsCollectionEnabled(true)
-
+        Analytics.setAnalyticsCollectionEnabled(true)
+        
         return true
     }
-
+    
     func application(_: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options _: UIScene.ConnectionOptions) -> UISceneConfiguration {
         UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
-
+    
     func application(_: UIApplication, didDiscardSceneSessions _: Set<UISceneSession>) {}
 }
