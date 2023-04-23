bcd6c173433fc76928addd66ca4ed71d4b27f0d7
Loïc Mazuc <mazuc.loic@icloud.com>
2023-04-23 16:19:05 +0200

    feat: create GridExamTypeViewController

--- Plome/Simulation/Simulation/ExamTypePageViewController.swift
+++ Plome/Simulation/Simulation/ExamTypePageViewController.swift
@@ -14,14 +14,14 @@ final class ExamTypePageViewController: TabmanViewController {
     
     // MARK: - Properties
     
-    private var viewControllers: [UIViewController] = []
+    private var viewControllers: [GridExamTypeViewController] = []
     private let titles: [String]
     
     // MARK: - UI
     
     private let bar: TMBar = TMBar.ButtonBar().configure { bar in
         bar.layout.transitionStyle = .progressive
-        bar.backgroundView.style = .clear
+        bar.backgroundView.style = .flat(color: PlomeColor.background.color)
         bar.indicator.tintColor = .black
         bar.indicator.weight = .light
         
@@ -33,15 +33,22 @@ final class ExamTypePageViewController: TabmanViewController {
         }
     }
     
+    private lazy var layout: UICollectionViewFlowLayout = .init().configure {
+        $0.itemSize = CGSize(width: (view.frame.width / 3) - AppStyles.defaultSpacing(factor: 2),
+                             height: (view.frame.width / 3) - AppStyles.defaultSpacing(factor: 2))
+        $0.minimumLineSpacing = AppStyles.defaultSpacing
+        $0.minimumInteritemSpacing = 1
+        $0.scrollDirection = .vertical
+        $0.sectionInset = .init(top: AppStyles.defaultSpacing,
+                                left: AppStyles.defaultSpacing(factor: 0.5),
+                                bottom: AppStyles.defaultSpacing,
+                                right: AppStyles.defaultSpacing(factor: 0.5))
+    }
+    
     // MARK: - Init
     
     required init(titles: [String]) {
         self.titles = titles
-        
-        for _ in 0..<titles.count {
-            viewControllers.append(.init())
-        }
-        
         super.init(nibName: nil, bundle: nil)
     }
     
@@ -53,6 +60,10 @@ final class ExamTypePageViewController: TabmanViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        for _ in 0..<titles.count {
+            viewControllers.append(.init(collectionViewLayout: layout))
+        }
         
         self.dataSource = self        
         addBar(bar, dataSource: self, at: .top)
@@ -74,5 +85,34 @@ extension ExamTypePageViewController: PageboyViewControllerDataSource, TMBarData
     
     func barItem(for bar: Tabman.TMBar, at index: Int) -> Tabman.TMBarItemable {
         TMBarItem(title: titles[index])
+    }
+}
+
+final class GridExamTypeViewController: UICollectionViewController {
+    
+    // MARK: - Properties
+    
+    private let reuseIdentifier: String = "cell"
+    
+    // MARK: - UI
+    
+    // MARK: - Life cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        collectionView.backgroundColor = PlomeColor.background.color
+        collectionView.register(UICollectionViewCell.self,
+                                forCellWithReuseIdentifier: reuseIdentifier)
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        // Should return number of exam for section type
+        return 12
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath).configure {
+            $0.backgroundColor = .blue
+        }
     }
 }
